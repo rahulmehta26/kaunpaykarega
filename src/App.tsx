0967a2e35f42051a2624a85365afcf9fa2079c5b
rahulmehta26 <rk6658542@gmail.com>
2025-08-07 16:12:27 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-amber-500 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
